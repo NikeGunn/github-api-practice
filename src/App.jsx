986012f659edb24ec63f6a3354d3b6b6edf9b33e
NIkhil <knewboy.nykhil@gmail.com>
2023-12-20 14:03:50 +0545

    Practising fetching data from github

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+// import GitHubCard from './components/GitHUbCard'
+import NikhilApiPractise from './components/GithubApi'
+
+function App() {
+
+  return (
+    <>
+     <NikhilApiPractise />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/GithubApi.jsx
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Card } from 'react-bootstrap';
+
+const GitHubCard = () => {
+  const [userData, setUserData] = useState([]);
+
+  useEffect(() => {
+    const fetchGitHubData = async () => {
+        //Fetch data from GitHub API
+            const username = 'NikeGunn';
+            const apiUrl = `https://api.github.com/users/${username}`;
+        // Handling errors with try/catch
+        try {
+            const response = await fetch(apiUrl);
+            const data = await response.json();
+            // console.log(data);
+            setUserData(data);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
+    fetchGitHubData();
+  }, []);
+
+
+
+
+// Mapping all the data from the API and showing to the page
+  // return (
+  //   <div>
+  //      {
+  //     userData.map((user) => {
+  //       return (
+          
+  //         <div key={user.id}>
+  //           <h1>GitHub User Card</h1>
+  //           <Card.Img variant="top" src={user.avatar_url} alt="GitHub Avatar" />
+  //           <Card.Body>
+  //             <Card.Title>{user.login}</Card.Title>
+  //             <Card.Text>{user.bio || 'No bio available.'}</Card.Text>
+  //             <Card.Text>Followers: {user.followers}</Card.Text>
+  //             <Card.Text>Following: {user.following}</Card.Text>
+  //             <Card.Text>Type: {user.type}</Card.Text>
+  //           </Card.Body>
+  //         </div>
+  //       )
+  //     })  
+  //   }
+  //   </div>
+  // );
+
+
+
+  return (
+    <Card style={{ width: '18rem' }}>
+      {userData && (
+        <>
+          <Card.Img variant="top" src={userData.avatar_url} alt="GitHub Avatar" />
+          <Card.Body>
+            <Card.Title>{userData.login}</Card.Title>
+            <Card.Text>{userData.bio || 'No bio available.'}</Card.Text>
+            <Card.Text>Followers: {userData.followers}</Card.Text>
+            <Card.Text>Following: {userData.following}</Card.Text>
+          </Card.Body>
+        </>
+      )}
+    </Card>
+  );
+};
+
+
+export default GitHubCard;
